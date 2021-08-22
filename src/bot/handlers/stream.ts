f5d8902a522b5be2be1d5baf7f6e12cbf8776b4d
Adam Raaif Nasheed <adam.raaif.nasheed7@gmail.com>
2021-08-22 17:56:20 +0500

    Fix: YT search only uses first word
    Also adds back custom() handler, usable by passing -c[ustom] as first arg

--- src/bot/handlers/stream.ts
+++ src/bot/handlers/stream.ts
@@ -1,7 +1,7 @@
 import ytdl from "ytdl-core";
 import { Composer } from "grammy";
 import i18n from "../i18n";
-import { audio, youtube, youtubeSearch } from "../streamer";
+import { audio, custom, youtube, youtubeSearch } from "../streamer";
 
 const composer = new Composer();
 
@@ -19,12 +19,23 @@ composer.command(["stream", "s", "play", "p"], async (ctx) => {
         return;
     }
 
-    const result =
-        typeof input === "string"
-            ? ytdl.validateURL(input)
-                ? await youtube(ctx.chat.id, ctx.from!, input)
-                : await youtubeSearch(ctx.chat.id, ctx.from!, input)
-            : await audio(ctx.message?.reply_to_message!);
+    let result;
+    if (typeof input === "string") {
+        if (ytdl.validateURL(input)) {
+            result = await youtube(ctx.chat.id, ctx.from!, input);
+        } else {
+            const customKeywords = ["-c", "-custom"];
+            let inputWords = ctx.message!.text.split(/\s/);
+            inputWords.shift();     // Remove command
+            if (customKeywords.includes(inputWords[0].toLowerCase())) {
+                result = await custom(inputWords[1], ctx.message!);
+            } else {
+                result = await youtubeSearch(ctx.chat.id, ctx.from!, inputWords.join(" "));
+            }
+        }
+    } else {
+        result = await audio(ctx.message?.reply_to_message!);
+    }
 
     if (result == null) {
         await ctx.reply(i18n("streaming"));
